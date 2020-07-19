91df80821183b2ae9e20324cbc124ce082f169b3
Spencer Kenealy <kenealys@me.com>
2020-07-19 14:08:59 -0700

    Contact Section Added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 // Components
 import Banner from './components/Banner/Banner';
 import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
 
 const App = () => {
   return (
     <div>
       <Banner />
       <Projects />
+      <Contact />
     </div>
   );
 };
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = () => {
+  return (
+    <div className='contact-container'>
+      <h1>Contact</h1>
+      <p>My name is Spencer Kenealy and I am currently looking for employment as a React developer. Please feel free to contact me any time!</p>
+      <div className='contact-info'>
+        <p>(310) 728-5515</p>
+        <p>kenealys@me.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
